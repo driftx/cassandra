9810998d5d5125ed2df9c8c53c84cb330fab6bfe
maxwellguo <cclive1601@gmail.com>
2024-04-08 23:15:20 +0800

    CommitlogArchiver only has granularity to seconds for restore_point_in_time for CASSANDRA-19448 5.0

--- src/java/org/apache/cassandra/db/commitlog/CommitLog.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLog.java
@@ -82,7 +82,7 @@ public class CommitLog implements CommitLogMBean
 
     final public AbstractCommitLogSegmentManager segmentManager;
 
-    public final CommitLogArchiver archiver;
+    public CommitLogArchiver archiver;
     public final CommitLogMetrics metrics;
     final AbstractCommitLogService executor;
 
@@ -403,7 +403,13 @@ public class CommitLog implements CommitLogMBean
     @Override
     public long getRestorePointInTime()
     {
-        return archiver.restorePointInTime;
+        return archiver.restorePointInTimeInMicros;
+    }
+
+    @VisibleForTesting
+    public void setCommitlogArchiver(CommitLogArchiver archiver)
+    {
+        this.archiver = archiver;
     }
 
     @Override

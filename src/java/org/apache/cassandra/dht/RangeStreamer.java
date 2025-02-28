d39ce5d0880877154af68628dab464b0f41e111f
Runtian Liu <curlylrt@gmail.com>
2025-02-28 14:30:58 -0800

    Only truncate data when there is SSTable

--- src/java/org/apache/cassandra/dht/RangeStreamer.java
+++ src/java/org/apache/cassandra/dht/RangeStreamer.java
@@ -700,7 +700,7 @@ public class RangeStreamer
                 if (RESET_BOOTSTRAP_PROGRESS.getBoolean())
                 {
                     SystemKeyspace.resetAvailableStreamedRangesForKeyspace(keyspace);
-                    Keyspace.truncateBlockingWithoutSnapshot(keyspace);
+                    Keyspace.truncateBlockingWithoutSnapshotIfDataExists(keyspace);
                     remaining = new ArrayList<>(fetchReplicas);
                 }
                 else
